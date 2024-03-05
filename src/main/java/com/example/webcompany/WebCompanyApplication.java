69beee070e41febb5862dfdb4d9377d25ed60152
Rameray1 <rameray1prog@gmail.com>
2024-03-05 14:19:52 +0100

    Initial commit

---
+++ src/main/java/com/example/webcompany/WebCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.webcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webcompany/WebCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
